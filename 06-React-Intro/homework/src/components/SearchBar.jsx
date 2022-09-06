489a9c371ac47a69872bfdc7f4e7826df5523deb
Juan Pablo Evangelista <juanpicap23@gmail.com>
2022-09-06 09:54:18 -0300

    homework react intro revised

--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -4,7 +4,12 @@ export default function SearchBar(props) {
   // acá va tu código
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Busca tu ciudad.."
+      />
       <button type="button" onClick={props.onSearch}>
         Agregar
       </button>
